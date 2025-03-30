e436e4291a691b65c579b2a747a66448484576c3
Manuel de Prada <manueldeprada@gmail.com>
2025-03-30 17:18:47 +0200

    u

--- replicate_experiment.py
+++ replicate_experiment.py
@@ -56,24 +56,42 @@ def compute_gflownet(temp):
     print(f"results: {final_results}")
     return final_results
 
+def compute_transition_scores_nonbeam(sequences, scores, normalize_logits=True):
+    if len(sequences.shape) == 1:
+        sequences = sequences.unsqueeze(0)
+    scores = torch.stack(scores, dim=1) # batch_size x seq_len x vocab_size
+    if normalize_logits:
+        scores = scores.log_softmax(dim=-1)
+    sequences = sequences[:, 1:]
+    transition_scores = scores.gather(dim=-1, index=sequences.unsqueeze(-1)).squeeze(-1)
+    return transition_scores
+
 def compute_huggingface(temp):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = AutoModelForCausalLM.from_pretrained("gpt2-xl", device_map="auto")
     tokenizer = AutoTokenizer.from_pretrained("gpt2-xl")
     prompts = get_prompts()
     results = np.zeros((len(prompts), 4))
-    for i, prompt in tqdm(enumerate(prompts), total=len(prompts), leave=False):
+    for i, prompt in enumerate(prompts):
         print(f"Prompt {i + 1}. ", end="")
-        samples, log_pf = model.generate(
-            *tokenizer(prompt, return_tensors="pt"),
+        inputs = tokenizer(prompt, return_tensors="pt").to(device)
+        prompt_len = inputs.input_ids.shape[1]
+        output = model.generate(
+            **inputs,
             do_sample=True,
             temperature=temp,
             top_k=0,
-            max_length=30,
-            num_return_sequences=10
+            max_new_tokens=30,
+            num_return_sequences=10,
+            return_dict_in_generate=True,
+            output_scores=True,
         )
-        samples_diversity, samples_det = compute_diversity(samples)
-        samples_likelihood = log_pf.sum(dim=-1)
+        samples_diversity, samples_det = compute_diversity(tokenizer.batch_decode(output.sequences[:, prompt_len:]))
+        samples_likelihood = compute_transition_scores_nonbeam(
+            output.sequences[:, prompt_len:],
+            output.scores,
+            normalize_logits=True
+        ).sum(dim=1)
         avg_likelihood = samples_likelihood.mean().item()
         max_likelihood = samples_likelihood.max().item()
         results[i] = np.array([samples_diversity, samples_det, avg_likelihood, max_likelihood])
@@ -82,21 +100,44 @@ def compute_huggingface(temp):
     return final_results
 
 
-def main():
+def gflownet():
     results = []
     for temp in temp_to_name.keys():
         print(f"Temperature: {temp}")
         out = compute_gflownet(temp)
         results.append([
+            "gflownet",
             temp,
             out[0],
             out[1],
             out[2],
             out[3]
         ])
-    df_results = pd.DataFrame(results, columns=["temp", "diversity", "det", "avg_likelihood", "max_likelihood"])
+    df_results = pd.DataFrame(results, columns=["type", "temp", "diversity", "det", "avg_likelihood", "max_likelihood"])
     df_results.to_csv("gflownet_results.csv", index=False)
 
+def huggingface():
+    results = []
+    for temp in temp_to_name.keys():
+        print(f"Temperature: {temp}")
+        out = compute_huggingface(temp)
+        results.append([
+            "ancestral",
+            temp,
+            out[0],
+            out[1],
+            out[2],
+            out[3]
+        ])
+    df_results = pd.DataFrame(results, columns=["type", "temp", "diversity", "det", "avg_likelihood", "max_likelihood"])
+    print(df_results)
+    saved_csv = pd.read_csv("gflownet_results.csv")
+    # add type column if not present
+    if "type" not in saved_csv.columns:
+        saved_csv["type"] = "gflownet"
+    df_results = pd.concat([saved_csv, df_results])
+    df_results.to_csv("gflownet_results.csv", index=False)
 
 if __name__ == "__main__":
-    main()
+    # gflownet()
+    huggingface()
